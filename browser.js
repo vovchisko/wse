b8393f36ca2bf3b36aa03ff3d64dcc87f3dabf09
Vladimir Ishenko <vovchisko@gmail.com>
2018-08-16 04:58:58 +0300

    hot browser client fix

--- browser.js
+++ browser.js
@@ -1,15 +1,3 @@
-let ws = null;
-
-if (typeof WebSocket !== 'undefined') {
-    ws = WebSocket
-} else if (typeof MozWebSocket !== 'undefined') {
-    ws = MozWebSocket
-} else if (typeof global !== 'undefined') {
-    ws = global.WebSocket || global.MozWebSocket
-} else if (typeof window !== 'undefined') {
-    ws = window.WebSocket || window.MozWebSocket
-} else if (typeof self !== 'undefined') {
-    ws = self.WebSocket || self.MozWebSocket
-}
-
-module.exports = ws;
+module.exports = {
+    WseClient: require('./src/client')
+};
