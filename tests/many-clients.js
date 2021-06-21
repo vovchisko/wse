d00f5861ff682869a8e902498de5c817c915c289
Vladimir Ishenko <vovchisko@gmail.com>
2021-06-21 17:14:01 +0300

    bump a-signal

--- tests/many-clients.js
+++ tests/many-clients.js
@@ -2,7 +2,7 @@ import { execute } from 'test-a-bit'
 
 import { create_pair, VALID_SECRET } from './_helpers.js'
 
-execute('connect and ready', async (success, fail) => {
+execute('//todo: many clients', async (success, fail) => {
   const { server, client } = create_pair()
 
   server.init()
